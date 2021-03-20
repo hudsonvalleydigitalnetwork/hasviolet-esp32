0c387663fbbd0621673efe048e36e059cf18d6b2
Joe Cupano <9505289+joecupano@users.noreply.github.com>
2021-03-20 17:18:30 -0400

    Sample config file

--- src/HASviolet_config.h
+++ src/HASviolet_config.h
@@ -8,9 +8,9 @@
 #define WIFI_APKEY "purple"
 
 //Uncomment and edit the next two lines if connecting to existing WiFi network
-#define WIFI_SSID "Excelsior"
-#define WIFI_KEY "19144966520"
+#define WIFI_SSID "HomeWAN"
+#define WIFI_KEY "2125551212"
 
 //Client credentials
 #define WWW_USER "radio"
-#define WWW_KEY "admin"
+#define WWW_KEY "radio"
